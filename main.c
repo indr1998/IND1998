4273e0d1f6b66c0a3ff4ab512f7d73cd30f76bf3
IND1998 <indranil.raj27@gmail.com>
2022-03-14 15:55:35 +0530

    new function for ading numbers

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+int add(int a,int b);
+
+
 int main(){
           printf("helloworld\n");
           return 0;
+}
+
+int add(int a,int b){
+        int c=a+b;
+        return c;
+}
 
